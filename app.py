f119a884de4bb2c07f2dd141cb08fb7639449560
Deborah'syemojarepo <deborah.aina@outlook.com>
2020-08-10 21:34:51 -0400

    Updating database

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #Save reference to tables in the db
-Stocks_table = Base.classes.stocks
+#Stocks_table = Base.classes.trending_stocks
 
 
 
@@ -69,7 +69,7 @@ def techincal_skills():
 def stock_analysis_():
     conn = engine.connect()
     #create list of columns
-    stocks_data = pd.read_sql("SELECT * FROM Stocks_table", conn)
+    stocks_data = pd.read_sql("SELECT * FROM trending_stocks", conn)
     stocks_data_json = stocks_data.to_dict(orient="records")
     return jsonify(stocks_data_json)
 
@@ -81,7 +81,7 @@ def preferredStocks(sample):
 
     # Filter the data based on the ticker submitted by the user
     #AMD, SHOP, ADP, GE etc
-    stocks_data = pd.read_sql("SELECT * FROM Stocks_table", conn)
+    stocks_data = pd.read_sql("SELECT * FROM trending_stocks", conn)
     stocks_data = stocks_data.loc[stocks_data["Ticker"] == choice]
     stocksChosen_json = stocks_data.to_dict(orient="records")
 
